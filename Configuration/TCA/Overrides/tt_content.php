5a0e13b7917129d9af3f4642449a2ff81e518808
Arne Lorenz <me@arnelorenz.de>
2022-06-13 10:58:41 +0200

    [FEATURE] initial base file changes for typo3 11

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,14 +2,21 @@
 defined('TYPO3_MODE') || die();
 
 /***************
- * Add flexForms for content element configuration
+ * FlexForms
  */
 $pluginSignature = 'apdocchecklogin_doccheckauthentication';
-
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-  $pluginSignature,
-  'FILE:EXT:ap_docchecklogin/Configuration/FlexForms/setup.xml'
+    $pluginSignature,
+    'FILE:EXT:ap_docchecklogin/Configuration/FlexForms/Setup.xml'
+);
+
+/***************
+ * Plugin
+ */
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Antwerpes.ApDocchecklogin',
+    'DocCheckAuthentication',
+    'LLL:EXT:ap_docchecklogin/Resources/Private/Language/locallang_db.xlf:pluginName'
 );
